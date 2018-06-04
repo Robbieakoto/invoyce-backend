ea83b70181288200567db6abccdd182b7b584381
Robbie <akotoro22@gmail.com>
2018-06-04 16:22:11 +0000

    updated resgistration and login routes

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
  //include express framework
  const express = require('express')
-
- //include body-parser for form request
+     //include body-parser for form request
  const bodyParser = require('body-parser');
-
  //include sqlite3 for the database
  const sqlite3 = require('sqlite3').verbose();
+ //include bcrypt module
+ const bcrypt = require('bcrypt');
 
  //set application to run on port 5000;
  const PORT = process.env.PORT || 5000;
-
  //instantiate express
  const app = express();
- const bcrypt = require('bcrypt')
  const saltRounds = 10;
+
 
  // disable urlencoded form request
  app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,47 +26,83 @@
      res.send("Welcome to invoyce ");
  });
 
+
+
  app.post('/register', (req, res) => {
-     if (req.body.name || (req.body.email) ||
-         (req.body.company_name) || (req.body.password)) {
+
+     //connecting to the database
+     let db = new sqlite3.Database("./database/invoyce.db");
+
+     //check to make sure none of the field/fields are empty
+     if (req.body.name == '' || req.body.email == '' ||
+         req.body.company_name == '' || req.body.password == '') {
          return res.json({
              'status': false,
              'message': 'All fields are required'
          });
-     }
+     } else {
+         //check if email already exists
 
-     //hashning password 
-     bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+         let column = `SELECT * from users where email='${req.body.email}'`;
+         db.all(column, [], (err, rows) => {
 
-         //connecting to the database
-         let db = new sqlite3.Database("./database/InvoicingApp.db");
-
-         //storing a query in the variable sql
-         let sql = `INSERT INTO users(name,email,company_name,password) VALUES('${
-          req.body.name
-        }','${req.body.email}','${req.body.company_name}','${hash}')`;
-
-         //running the query
-         db.run(sql, function(err) {
              if (err) {
-                 //throw an error if sql failed
                  throw err;
+             }
+             if (rows.length != 0) {
+                 return res.json({
+                     status: false,
+                     message: "user already exists"
+                 });
              } else {
 
-                 //return json if sql successfull
-                 return res.json({
-                     status: true,
-                     message: "User Created"
+                 //creates a user if no email exists
+                 bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+                     let sql = `INSERT INTO users(name,email,company_name,password) VALUES('${
+                    req.body.name
+                  }','${req.body.email}','${req.body.company_name}','${hash}')`;
+                     db.run(sql, (err) => {
+                         if (err) {
+                             throw err;
+                         } else {
+                             //creates user
+                             return res.json({
+                                 status: true,
+                                 message: "user created"
+                             });
+                         }
+                     })
                  });
              }
          });
-         //closes the database connection
-         db.close();
-     });
+     }
+     // db.close();
  });
 
+
+ //hashing password 
+
  app.post('/login', (req, res) => {
-     res.send("Login here");
+
+     //connects to the database
+     let db = new sqlite3.Database("./database/invoyce.db");
+
+     //store a query in sql
+     let sql = `SELECT * from users where email='${req.body.email}'`;
+     db.all(sql, [], (err, rows) => {
+         if (err) {
+             throw err;
+         }
+         db.close();
+
+         //check if email is correct
+         if (rows.length == 0) {
+             return res.json({
+                 status: false,
+                 message: "Sorry, wrong email"
+             });
+         }
+     });
  });
 
  app.post('/invoice', (req, res) => {
